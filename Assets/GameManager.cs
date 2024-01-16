877e282be12f9e99e8b40712b1fa32db75434355
eric [itb] rodriguez luque <eric.rodriguez.7e6@itb.cat>
2024-01-16 15:59:31 +0100

    Shop sacada del GameManager
    + Ahora la tienda tiene su propio script y funciona por su cuenta, no se ejecuta en el gamemanager constantemente

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -22,7 +22,7 @@ public class GameManager : MonoBehaviour
     public GameObject gameOver;
     public GameObject mainMenu;
     public GameObject shop;
-    public List<GameObject> shopButtons;
+    public GameObject achievements;
     private float startDate;
     public Toggle fullScreen;
 
@@ -70,15 +70,6 @@ public class GameManager : MonoBehaviour
 
         gameOver.SetActive(false);
         isDead = true;
-
-        foreach (var item in shop.GetComponentsInChildren<Transform>())
-        {
-            if(item.name.Length >= 6 && item.name[..6] == "Button")
-            {
-                shopButtons.Add(item.gameObject);
-            }
-        }
-        shop.SetActive(false);
     }
 
     private void Update()
@@ -96,18 +87,6 @@ public class GameManager : MonoBehaviour
                 }
                 UpdateScore();
             }
-        }
-        if(shop.activeSelf)
-        {
-            EditText(0, $"Shields: {data.shields}\n 5 Coins");
-            EditText(1, $"ExtraJumps: {data.extraJumps}\n 100 Coins");
-            EditText(2, $"Less Color Cooldown: {data.lessCooldownColorChange}\n 50 Coins");
-            EditText(3, $"Don't lose Color: {data.dontLoseColorAtShoot}\n 125 Coins");
-            EditText(4, $"Piercing Bullets: {data.bulletPenetration}\n 50 Coins");
-            EditText(5, $"Longer Invulnerability: {data.longerInvulnerability}\n 50 Coins");
-            EditText(6, $"Bigger Bullets: {data.biggerBullets}\n 75 Coins");
-            EditText(7, $"Double Coins (Consum): {data.doubleCoinsAtCollect}\n 12 Coins");
-            EditText(8, $"Invulnerability (Consum): {data.marioStar}\n 15 Coins");
         }
     }
 
@@ -283,10 +262,6 @@ public class GameManager : MonoBehaviour
         }
 
         UpdateCoins();
-    }
-    private void EditText(int index, string text)
-    {
-        shopButtons[index].GetComponentInChildren<TextMeshProUGUI>().text = text;
     }
     public void SetResolution(Resolution resolution)
     {
